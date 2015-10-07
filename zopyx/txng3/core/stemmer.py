91e7331b2411f17f84d71fc25a8d0e5557cec0b4
Radim Novotny <novotny.radim@gmail.com>
2015-10-07 14:19:23 +0200

    Added Czech stemmer support

--- zopyx/txng3/core/stemmer.py
+++ zopyx/txng3/core/stemmer.py
@@ -1,5 +1,5 @@
 ###########################################################################
-# TextIndexNG V 3                
+# TextIndexNG V 3
 # The next generation TextIndex for Zope
 #
 # This software is governed by a license. See
@@ -13,32 +13,32 @@ from zopyx.txng3.ext import stemmer
 # maps country codes to language name
 country_codes = {
 
-    'da' : 'danish', 
-    'nl' : 'dutch', 
-    'en' : 'english', 
-    'fi' : 'finnish', 
-    'fr' : 'french', 
-    'de' : 'german', 
-    'hu' : 'hungarian', 
-    'it' : 'italian', 
-    'no' : 'norwegian', 
-    'pt' : 'portuguese', 
-    'ro' : 'romanian',
-    'ru' : 'russian', 
-    'es' : 'spanish', 
-    'sv' : 'swedish',
-    'tr' : 'turkish',
+    'cs': 'czech',
+    'da': 'danish',
+    'nl': 'dutch',
+    'en': 'english',
+    'fi': 'finnish',
+    'fr': 'french',
+    'de': 'german',
+    'hu': 'hungarian',
+    'it': 'italian',
+    'no': 'norwegian',
+    'pt': 'portuguese',
+    'ro': 'romanian',
+    'ru': 'russian',
+    'es': 'spanish',
+    'sv': 'swedish',
+    'tr': 'turkish',
 }
 
 availableStemmers = stemmer.availableStemmers()
 
+
 def getStemmer(language):
-    """ Return a Stemmer instance for a given language or country code 
+    """ Return a Stemmer instance for a given language or country code
         or None if no stemmer exists.
     """
-
-    original_language = language
-    if not language in availableStemmers:
+    if language not in availableStemmers:
         language = country_codes.get(language)
     if language:
         return stemmer.Stemmer(language)
